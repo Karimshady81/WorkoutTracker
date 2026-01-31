1caf1898fa2d5495616f617fc629f6a01ed535e5
Karim shady <kshady960@gmail.com>
2026-01-31 21:58:17 +0200

    feat(domain): add WorkoutSession entity

---
+++ WorkoutTracker.Domain/Entities/WorkoutSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Domain.Entities
+{
+    public class WorkoutSession
+    {
+        public Guid Id { get; set; }
+        public DateTime PerformedAt { get; set; }
+    }
+}
