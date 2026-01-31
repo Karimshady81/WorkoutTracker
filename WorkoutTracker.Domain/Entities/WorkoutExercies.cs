1533002717a42a163da571852e268a8556755366
Karim shady <kshady960@gmail.com>
2026-01-31 21:06:37 +0200

    feat(domain): add WorkoutExercises entity

---
+++ WorkoutTracker.Domain/Entities/WorkoutExercies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Domain.Entities
+{
+    public class WorkoutExercies
+    {
+        public Guid Id { get; set; }
+        public int OrderInWorkout { get; set; }
+        public int TargetSets { get; set; }
+        public int TargetReps { get; set; }
+        public int RestSeconds { get; set; }
+    }
+}
