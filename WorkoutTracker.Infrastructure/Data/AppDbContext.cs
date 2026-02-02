41e38fb218a661132eae97f46122fa03eb40a4fe
Karim shady <kshady960@gmail.com>
2026-02-02 16:31:19 +0200

    feat(infrastructure): Add DbContext class foundation (Dbset)

--- WorkoutTracker.Infrastructure/Data/AppDbContext.cs
+++ WorkoutTracker.Infrastructure/Data/AppDbContext.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
 
 namespace WorkoutTracker.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
+        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+        public DbSet<ExerciseSet> ExercisesSets { get; set; }
     }
 }
