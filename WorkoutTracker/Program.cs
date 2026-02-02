e11ab0bcc3dd387e478c3b37f346c4d7528c7db7
Karim shady <kshady960@gmail.com>
2026-02-02 16:49:42 +0200

    feat(infrastructure): Register the DbContext in the program

--- WorkoutTracker/Program.cs
+++ WorkoutTracker/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using WorkoutTracker.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var conncetionString = builder.Configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseMySql(conncetionString, ServerVersion.AutoDetect(conncetionString));
+});
 
 var app = builder.Build();
 
