ab63c28960846b9ff47e73c182d7795bdd8e20d7
Karim shady <kshady960@gmail.com>
2026-02-02 21:26:33 +0200

    feat(infrastructure): Add ExerciseConfiguration for Database

---
+++ WorkoutTracker.Infrastructure/Data/Configurations/ExerciseConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Infrastructure.Data.Configurations
+{
+    public class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
+    {
+        public void Configure(EntityTypeBuilder<Exercise> entity)
+        {
+            entity.ToTable("Exercises");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            entity.Property(e => e.MuscleGroup)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            entity.Property(e => e.Equipment)
+                   .IsRequired()
+                   .HasMaxLength(100);
+        }
+    }
+}
