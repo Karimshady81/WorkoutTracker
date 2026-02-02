e0edeba7589a58cb8cbcf15d21506bdb79d17aa1
Karim shady <kshady960@gmail.com>
2026-02-02 22:19:57 +0200

    feat(infrastructure): Add WorkoutSessionConfiguration for Database

---
+++ WorkoutTracker.Infrastructure/Data/Configurations/WorkoutSessionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Infrastructure.Data.Configurations
+{
+    public class WorkoutSessionConfiguration : IEntityTypeConfiguration<WorkoutSession>
+    {
+        public void Configure(EntityTypeBuilder<WorkoutSession> entity)
+        {
+            entity.ToTable("WorkoutSessions");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.PerformedAt)
+                    .HasDefaultValue("CURRENT_TIMESTAMP");
+
+            entity.HasOne(e => e.Workout)
+                  .WithMany(e => e.WorkoutSessions)
+                  .HasForeignKey(e => e.WorkoutId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
