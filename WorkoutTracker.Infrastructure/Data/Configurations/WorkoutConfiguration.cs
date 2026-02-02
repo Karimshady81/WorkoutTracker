0891d8800b0ad618f0c9a4af4afa35c8ecc8c722
Karim shady <kshady960@gmail.com>
2026-02-02 21:12:13 +0200

    feat(infrastructure): Add WorkoutConfiguration for Database

---
+++ WorkoutTracker.Infrastructure/Data/Configurations/WorkoutConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Infrastructure.Data.Configurations
+{
+    public class WorkoutConfiguration : IEntityTypeConfiguration<Workout>
+    {
+        public void Configure(EntityTypeBuilder<Workout> entity)
+        {
+            entity.ToTable("Workouts");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Name)
+                  .IsRequired()
+                  .HasMaxLength(200);
+
+            entity.Property(e => e.Description)
+                   .HasMaxLength(255);
+
+            entity.Property(e => e.CreatedAt)
+                   .HasDefaultValue("CURRENT_TIMESTAMP");
+        }
+    }
+}
