6e57ae1e3cae858dc4bbb92790575010c33b96f2
Karim shady <kshady960@gmail.com>
2026-02-02 20:47:07 +0200

    chore(infra): update class name

--- WorkoutTracker.Infrastructure/Data/Configurations/UserConfiguration.cs
+++ WorkoutTracker.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -4,7 +4,7 @@ using WorkoutTracker.Domain.Entities;
 
 namespace WorkoutTracker.Infrastructure.Data.Configurations
 {
-    public class UserConfugration : IEntityTypeConfiguration<User>
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> entity)
         {
