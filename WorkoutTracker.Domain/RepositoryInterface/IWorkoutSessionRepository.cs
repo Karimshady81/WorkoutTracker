a86ddde4cbbada2c9f82a541db7c5ca85f400d09
Karim shady <kshady960@gmail.com>
2026-02-04 17:28:51 +0200

    feat(infrastruture): Add IWorkoutSessionRepository

---
+++ WorkoutTracker.Domain/RepositoryInterface/IWorkoutSessionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Domain.RepositoryInterface
+{
+    public interface IWorkoutSessionRepository
+    {
+        Task<WorkoutSession?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<WorkoutSession>> GetByUserIdAsync(Guid userId);
+        Task AddAsync(WorkoutSession workoutSession);
+        void Delete(WorkoutSession workoutSession);
+    }
+}
