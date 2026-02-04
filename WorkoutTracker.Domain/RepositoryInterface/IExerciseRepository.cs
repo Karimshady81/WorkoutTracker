878d6a79e9784fc1cdbe0eb5635cd2119065cfae
Karim shady <kshady960@gmail.com>
2026-02-04 17:17:15 +0200

    feat(infrastruture): Add IExerciseRepository

---
+++ WorkoutTracker.Domain/RepositoryInterface/IExerciseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Domain.RepositoryInterface
+{
+    public interface IExerciseRepository
+    {
+        Task<Exercise?> GetByIdAsync(Guid id);
+        Task<Exercise?> GetByNameAsync(string name);
+        Task<IReadOnlyList<Exercise>> GetAllAsync();
+
+        Task AddAsync(Exercise exercise);
+        void DeleteAsync(Exercise exercise);
+    }
+}
