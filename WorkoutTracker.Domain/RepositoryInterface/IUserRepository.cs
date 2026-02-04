087cc4d84c6613247ab31ddc53ced957d9372ef9
Karim shady <kshady960@gmail.com>
2026-02-04 16:36:00 +0200

    feat(infrastruture): Add IUserRepository

---
+++ WorkoutTracker.Domain/RepositoryInterface/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Domain.RepositoryInterface
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByIdAsync(Guid Id); 
+        Task<User?> GetUserByEmailAsync(string email);
+        Task AddAsync(User user);
+    }
+}
