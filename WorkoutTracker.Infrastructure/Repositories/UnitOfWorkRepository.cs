54c32615243a39ccf710261a9bf19978f8b50a17
Karim shady <kshady960@gmail.com>
2026-02-07 22:45:04 +0200

    feature(Infrastructure): Implement IUnitOfWork Repository

---
+++ WorkoutTracker.Infrastructure/Repositories/UnitOfWorkRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.RepositoryInterface;
+using WorkoutTracker.Infrastructure.Data;
+
+namespace WorkoutTracker.Infrastructure.Repositories
+{
+    public class UnitOfWorkRepository : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWorkRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
