de043ad7b61f59956b11583385c4ea81347a9553
Karim shady <kshady960@gmail.com>
2026-02-09 23:23:31 +0200

    feature(application): ImplementAddExerciseSet method service

--- WorkoutTracker.Application/Services/WorkoutSessionService.cs
+++ WorkoutTracker.Application/Services/WorkoutSessionService.cs
@@ -10,12 +10,17 @@ namespace WorkoutTracker.Application.Services
     {
         private readonly IWorkoutRepository _workoutRepository;
         private readonly IWorkoutSessionRepository _workoutSessionRepository;
+        private readonly IExerciseRepository _exerciseRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public WorkoutSessionService(IWorkoutRepository workoutRepository, IWorkoutSessionRepository workoutSessionRepository, IUnitOfWork unitOfWork)
+        public WorkoutSessionService(IWorkoutRepository workoutRepository, 
+                                    IWorkoutSessionRepository workoutSessionRepository, 
+                                    IExerciseRepository exerciseRepository, 
+                                    IUnitOfWork unitOfWork)
         {
             _workoutRepository = workoutRepository;
             _workoutSessionRepository = workoutSessionRepository;
+            _exerciseRepository = exerciseRepository;
             _unitOfWork = unitOfWork;
         }
 
@@ -45,6 +50,43 @@ namespace WorkoutTracker.Application.Services
 
             //Return sessionId
             return session.Id;
+        }
+
+        public async Task<Guid> AddExerciseSetAsync(Guid userId, AddExerciseRequest request)
+        {
+            var session = await _workoutSessionRepository.GetByIdAsync(request.SessionId);
+
+            //Validate
+            if (session is null)
+                throw new InvalidOperationException("Workout session not found");
+
+            if (session.Workout.UserId != userId)
+                throw new UnauthorizedAccessException("You do not have permission to add an exercise set to this session");
+
+
+            var exercise = await _exerciseRepository.GetByIdAsync(request.ExerciseId);
+
+            if (exercise is null)
+                throw new InvalidOperationException("Exercise not found");
+
+            //Create exercise set
+            var exerciseSet = new ExerciseSet
+            {
+                Id = Guid.NewGuid(),
+                WorkoutSessionId = session.Id,
+                ExerciseId = exercise.Id,
+                Reps = request.Reps,
+                Weight = request.Weight,
+                SetNumber = request.SetNumber
+            };
+
+            //Attach to session (aggregate root)
+            session.ExerciesSets.Add(exerciseSet);
+
+            //Persist changes
+            await _unitOfWork.SaveChangesAsync();
+
+            return exerciseSet.Id;
         }
     }
 }
