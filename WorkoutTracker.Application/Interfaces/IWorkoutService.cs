247e9394483a4d47c6594cf5ccdf78771572ed48
Karim shady <kshady960@gmail.com>
2026-02-09 16:00:40 +0200

    feature(application): Add interfaces

---
+++ WorkoutTracker.Application/Interfaces/IWorkoutService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Application.DTOs;
+
+namespace WorkoutTracker.Application.Interfaces
+{
+    public interface IWorkoutService
+    {
+        Task<Guid> CreateWorkoutAsync(Guid userId, CreateWorkoutRequest request);
+    }
+}
