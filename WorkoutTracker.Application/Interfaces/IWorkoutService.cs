390d1b13352c98289bb4d89a829afbb9c63dd126
Karim shady <kshady960@gmail.com>
2026-02-09 16:54:14 +0200

    feature(infrastructure): Add GetWorkoutsByUser method

--- WorkoutTracker.Application/Interfaces/IWorkoutService.cs
+++ WorkoutTracker.Application/Interfaces/IWorkoutService.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WorkoutTracker.Application.DTOs;
+using WorkoutTracker.Application.DTOs.Requests;
+using WorkoutTracker.Application.DTOs.Response;
 
 namespace WorkoutTracker.Application.Interfaces
 {
     public interface IWorkoutService
     {
         Task<Guid> CreateWorkoutAsync(Guid userId, CreateWorkoutRequest request);
+        Task<IReadOnlyList<WorkoutListResponse>> GetWorkoutsByUserAsync(Guid userId);
     }
 }
