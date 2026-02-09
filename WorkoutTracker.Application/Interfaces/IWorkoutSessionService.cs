c1937aec2352d2afe7a4c66bdf202775df97e59d
Karim shady <kshady960@gmail.com>
2026-02-09 22:31:10 +0200

    feature(application): Add IWorkoutSession interface

---
+++ WorkoutTracker.Application/Interfaces/IWorkoutSessionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Application.DTOs.Requests;
+
+namespace WorkoutTracker.Application.Interfaces
+{
+    public interface IWorkoutSessionService
+    {
+        Task<Guid> CreateWorkoutSession(Guid userId,StartWorkoutSessionRequest request);
+    }
+}
