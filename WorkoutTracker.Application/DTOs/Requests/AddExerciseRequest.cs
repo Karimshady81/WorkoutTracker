1fe6e1da87d6892ac92a4a3fa2d79c35e49a7abd
Karim shady <kshady960@gmail.com>
2026-02-09 23:24:01 +0200

    feature(application): Add ExerciseRequest DTO

---
+++ WorkoutTracker.Application/DTOs/Requests/AddExerciseRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Application.DTOs.Requests
+{
+    public class AddExerciseRequest
+    {
+        public Guid SessionId { get; set; }
+        public Guid ExerciseId { get; set; }
+
+        public int SetNumber { get; set; }
+        public int Reps { get; set; }
+        public double Weight { get; set; }
+    }
+}
