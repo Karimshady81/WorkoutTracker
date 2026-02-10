5b7a38dc1e4f458efbd2b3067517182f87e5db35
Karim shady <kshady960@gmail.com>
2026-02-10 17:54:56 +0200

    chore(Domain): Update repository interface

--- WorkoutTracker.Domain/RepositoryInterface/IWorkoutSessionRepository.cs
+++ WorkoutTracker.Domain/RepositoryInterface/IWorkoutSessionRepository.cs
@@ -9,7 +9,8 @@ namespace WorkoutTracker.Domain.RepositoryInterface
 {
     public interface IWorkoutSessionRepository
     {
-        Task<WorkoutSession?> GetByIdAsync(Guid id); //View session details
+        Task<WorkoutSession?> GetByIdAsync(Guid id); //View session 
+        Task<WorkoutSession?> GetWithDetailsByIdAsync(Guid sessionId); //View session with exercises
         Task<IReadOnlyList<WorkoutSession>> GetByUserIdAsync(Guid userId); //Workout histroy
         Task AddAsync(WorkoutSession workoutSession); //start/log session
         void Delete(WorkoutSession workoutSession); //delete session
