e280a5a4e0d5b67c8aa5d8f8b7d4375a63f0158b
Karim shady <kshady960@gmail.com>
2026-02-11 17:09:58 +0200

    feature(Application): Add WorkoutSessionSummary DTO

---
+++ WorkoutTracker.Application/DTOs/Response/WorkoutSessionSummaryResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Application.DTOs.Response
+{
+    public class WorkoutSessionSummaryResponse
+    {
+        public Guid SessionId { get; set; }
+        public string WorkoutName { get; set; } = string.Empty;
+        public DateTime StartedAt { get; set; }
+        public DateTime? EndedAt { get; set; }
+    }
+}
