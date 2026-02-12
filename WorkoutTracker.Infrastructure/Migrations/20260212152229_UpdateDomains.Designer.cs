8d22727a731215192adcbfb106506e151444179d
Karim shady <kshady960@gmail.com>
2026-02-12 17:51:33 +0200

    chore(Infrastrucutre): Update migrations

--- WorkoutTracker.Infrastructure/Migrations/20260212152229_UpdateDomains.Designer.cs
+++ WorkoutTracker.Infrastructure/Migrations/20260212152229_UpdateDomains.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkoutTracker.Infrastructure.Data;
 
@@ -10,9 +11,11 @@ using WorkoutTracker.Infrastructure.Data;
 namespace WorkoutTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260212152229_UpdateDomains")]
+    partial class UpdateDomains
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WorkoutTracker.Infrastructure/Migrations/20260212152229_UpdateDomains.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDomains : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PerformedAt",
+                table: "WorkoutSessions",
+                newName: "StartedAt");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndedAt",
+                table: "WorkoutSessions",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndedAt",
+                table: "WorkoutSessions");
+
+            migrationBuilder.RenameColumn(
+                name: "StartedAt",
+                table: "WorkoutSessions",
+                newName: "PerformedAt");
+        }
+    }
+}
