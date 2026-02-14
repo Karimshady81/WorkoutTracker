3bb16363a21bb7b07a0274410b4c0e69ca6f0377
Karim shady <kshady960@gmail.com>
2026-02-14 20:45:15 +0200

    chore: Add and implement exerciseSetRepository

---
+++ WorkoutTracker.Domain/RepositoryInterface/IExerciseSetRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
+
+namespace WorkoutTracker.Domain.RepositoryInterface
+{
+    public interface IExerciseSetRepository
+    {
+        Task AddAsync(ExerciseSet exerciseSet);
+    }
+}
---
+++ WorkoutTracker.Infrastructure/Repositories/ExerciseSetRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Domain.Entities;
+using WorkoutTracker.Domain.RepositoryInterface;
+using WorkoutTracker.Infrastructure.Data;
+
+namespace WorkoutTracker.Infrastructure.Repositories
+{
+    public class ExerciseSetRepository : IExerciseSetRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ExerciseSetRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ExerciseSet exerciseSet)
+        {
+            await _context.ExercisesSets.AddAsync(exerciseSet);
+        }
+    }
+}
