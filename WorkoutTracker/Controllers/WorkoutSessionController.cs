057d33f283388cb0b111690b8c37b5aa747f1333
Karim shady <kshady960@gmail.com>
2026-02-14 20:48:00 +0200

    feature(API): Implement WorkoutSession controller

---
+++ WorkoutTracker/Controllers/WorkoutSessionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutTracker.Application.DTOs.Requests;
+using WorkoutTracker.Application.Interfaces;
+
+namespace WorkoutTracker.API.Controllers
+{
+    [Route("api/workout-session")]
+    [ApiController]
+    public class WorkoutSessionController : ControllerBase
+    {
+        private readonly IWorkoutSessionService _workoutSession;
+
+        public WorkoutSessionController(IWorkoutSessionService workoutSession)
+        {
+            _workoutSession = workoutSession;
+        }
+
+        [HttpPost("start")]
+        public async Task<IActionResult> StartWorkoutSession([FromQuery]Guid userId, [FromBody] StartWorkoutSessionRequest request)
+        {
+            var session = await _workoutSession.StartWorkoutSessionAsync(userId, request);
+
+            return Ok(session);
+        }
+
+        [HttpPost("add-exercises")]
+        public async Task<IActionResult> AddExercises([FromQuery] Guid userId, [FromBody] AddExerciseRequest request)
+        {
+            var exercisesAdded = await _workoutSession.AddExerciseSetAsync(userId, request);
+
+            return Ok(exercisesAdded);
+        }
+    }
+}
