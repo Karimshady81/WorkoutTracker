63aa43e9d19b7674b85bb92856ba440974e8f9e2
Karim shady <kshady960@gmail.com>
2026-02-16 22:41:05 +0200

    feature(Application): Add methods for Auth interface

---
+++ WorkoutTracker.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Application.DTOs.Requests;
+using WorkoutTracker.Application.DTOs.Response;
+
+namespace WorkoutTracker.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> RegisterUserAsync(RegisterRequest request);
+        Task<AuthResponse?> LoginUserAsync(LoginRequest request);
+    }
+}
