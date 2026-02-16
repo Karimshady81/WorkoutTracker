363e380dd875bf7469f81f19042544e49d08ff7f
Karim shady <kshady960@gmail.com>
2026-02-16 23:12:54 +0200

    feature(API): Register JWT security

--- WorkoutTracker/Program.cs
+++ WorkoutTracker/Program.cs
@@ -1,15 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WorkoutTracker.Application.Interfaces;
+using WorkoutTracker.Application.Security_Interface;
 using WorkoutTracker.Application.Services;
 using WorkoutTracker.Domain.RepositoryInterface;
 using WorkoutTracker.Infrastructure.Data;
 using WorkoutTracker.Infrastructure.Repositories;
+using WorkoutTracker.Infrastructure.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
 
 //Regiser repos
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IWorkoutRepository, WorkoutRepository>();
 builder.Services.AddScoped<IExerciseRepository, ExerciseRepository>();
