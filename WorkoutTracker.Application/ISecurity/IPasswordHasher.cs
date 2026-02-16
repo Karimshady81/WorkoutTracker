c6e6551ac64ad1565451460b637ad19c1db4e1dc
Karim shady <kshady960@gmail.com>
2026-02-16 23:11:05 +0200

    chore(Application): Change the location of security interface

---
+++ WorkoutTracker.Application/ISecurity/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Application.Security_Interface
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string password, string hashedPassword);
+    }
+}
---
+++ WorkoutTracker.Application/Services/AuthService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Application.DTOs.Requests;
+using WorkoutTracker.Application.DTOs.Response;
+using WorkoutTracker.Application.Interfaces;
+using WorkoutTracker.Domain.Entities;
+using WorkoutTracker.Domain.RepositoryInterface;
+
+using WorkoutTracker.Application.Security_Interface;
+
+namespace WorkoutTracker.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IJwtGenerator _jwtGenerator;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<AuthResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            //Validate inputs first
+            if (string.IsNullOrEmpty(request.Email))
+                throw new InvalidOperationException("Email is required.");
+
+            if (string.IsNullOrEmpty(request.Password))
+                throw new InvalidOperationException("Password is required.");
+
+            //Check if user already exists
+            if (await _userRepository.GetUserByEmailAsync(request.Email) != null)
+                throw new InvalidOperationException("User with this email already exists.");
+
+            //Map
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = _passwordHasher.Hash(request.Password)
+            };
+
+            //Generate JWT token
+            var token =  _jwtGenerator.GenerateToken(user.Id.ToString(), user.Email);
+
+            //Save to database
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.SaveChangesAsync();
+
+            return new AuthResponse
+            {
+                userId = user.Id,
+                Token = token
+            };
+        }
+
+        public Task<AuthResponse?> LoginUserAsync(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
